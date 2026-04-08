4cb17c48ee0eb652843bac201f9c612b0b0c15ef
Andrzej <a.wisniewski@hadronforbusiness.com>
2026-04-08 07:24:17 +0200

    normal commit::

--- hfb_xmlmap_exporter/__manifest__.py
+++ hfb_xmlmap_exporter/__manifest__.py
@@ -92,6 +92,7 @@
 		"data/communication_channel_demo.xml",
 	],
 	'images': [
+		'static/description/Banner.png',
 		'hfb_xmlmap_exporter/static/description/icon.png',
 	],
 	'icon': 'hfb_xmlmap_exporter/static/description/icon.png',
