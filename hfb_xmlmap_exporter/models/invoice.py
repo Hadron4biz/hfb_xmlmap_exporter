f1a6d2ad10d772cb46a745e0987ff1ca958fa822
Andrzej <a.wisniewski@hadronforbusiness.com>
2026-04-09 13:24:12 +0200

    normal commit::

--- hfb_xmlmap_exporter/models/invoice.py
+++ hfb_xmlmap_exporter/models/invoice.py
@@ -274,7 +274,7 @@ class AccountMove(models.Model):
 		_logger.info(f"🧩 Walidacja faktury {self.name} względem schemy XSD z szablonu {template.name}")
 
 		try:
-			xml_bytes = template.generate_xml(self, in_memory=True)
+			xml_bytes = template.sudo().generate_xml(self, in_memory=True)
 			if not xml_bytes:
 				raise UserError(_("Nie udało się wygenerować pliku XML z szablonu."))
 		except Exception as e:
@@ -512,7 +512,7 @@ class AccountMove(models.Model):
 		# 1. Generujemy XML (in_memory=True)
 		# ----------------------------------------------------------------------
 		try:
-			xml_bytes = template.generate_xml(self, in_memory=True)
+			xml_bytes = template.sudo().generate_xml(self, in_memory=True)
 			if not xml_bytes:
 				raise UserError(_("Nie udało się wygenerować XML z szablonu."))
 
