dc668dfc823516d9b43f6023168c3831cf105912
Andrzej <a.wisniewski@hadronforbusiness.com>
2026-04-09 13:20:09 +0200

    normal commit::

--- hfb_xmlmap_exporter/models/invoice.py
+++ hfb_xmlmap_exporter/models/invoice.py
@@ -30,7 +30,7 @@
 # solutions contained herein are not covered by this license and remain the
 # property of the author.
 #################################################################################
-"""@version 19.1.3
+"""@version 19.0.1
    @owner  Hadron for Business Sp. z o.o.
    @author Andrzej Wiśniewski (warp3r)
    @date   2026-03-07
@@ -47,6 +47,7 @@ from markupsafe import Markup, escape
 
 import logging
 _logger = logging.getLogger(__name__)
+
 
 """
 Faktura (account.move) 
@@ -285,7 +286,7 @@ class AccountMove(models.Model):
 		_logger.info(f"🧩 Walidacja faktury {self.name} względem schemy XSD z szablonu {template.name}")
 
 		try:
-			xml_bytes = template.generate_xml(self, in_memory=True)
+			xml_bytes = template.sudo().generate_xml(self, in_memory=True)
 			if not xml_bytes:
 				raise UserError(_("Nie udało się wygenerować pliku XML z szablonu."))
 		except Exception as e:
@@ -353,6 +354,7 @@ class AccountMove(models.Model):
 		except Exception as e:
 			_logger.warning(f"⚠️  Nie udało się zapisać logu walidacji: {e}")
 
+
 		return {
 			"type": "ir.actions.act_window",
 			"res_model": "account.move",
@@ -388,28 +390,6 @@ class AccountMove(models.Model):
 				"exec_reload": True,
 			},
 		}
-
-	# ------------------------------------------------------------------------------------------------------
-	# Wymuszenie wysyłki jeśli jest możliwe
-	# ------------------------------------------------------------------------------------------------------
-	def action_send_manual(self):
-		domain = [
-			( 'document_id', '=', self.id),
-			( 'document_model', '=', 'account.move' )
-		]
-		log = self.env['communication.log'].sudo().search(domain, limit=1)
-		comm_status = 'none'
-		if log:
-			comm_status = log.state
-			log.sudo().action_send_manual()
-
-		_logger.info(
-			f"\n action_send_manual document: {self.name}"
-			f"\n ksef_process_state {self.ksef_process_state} "
-			f'\n communication.log {log}'
-			f'\n comm_status {comm_status}'
-			f"\n___________________________________________________________________________________________"
-		)
 
 	########################################################################################################
 
@@ -546,7 +526,7 @@ class AccountMove(models.Model):
 		# 1. Generujemy XML (in_memory=True)
 		# ----------------------------------------------------------------------
 		try:
-			xml_bytes = template.generate_xml(self, in_memory=True)
+			xml_bytes = template.sudo().generate_xml(self, in_memory=True)
 			if not xml_bytes:
 				raise UserError(_("Nie udało się wygenerować XML z szablonu."))
 
@@ -659,6 +639,7 @@ class AccountMove(models.Model):
 		# ... do implementacji po analizie dalszej części schematu
 		
 		return fa_data
+
 
 
 #EoF
