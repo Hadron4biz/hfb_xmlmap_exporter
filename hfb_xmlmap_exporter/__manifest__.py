f0beeeab4d2958d02b4e72d99ec92cf5aa0c345c
Andrzej <a.wisniewski@hadronforbusiness.com>
2026-04-21 12:03:32 +0200

    normal commit::

--- hfb_xmlmap_exporter/__manifest__.py
+++ hfb_xmlmap_exporter/__manifest__.py
@@ -31,7 +31,7 @@
 # property of the author.
 #################################################################################
 {
-    "name": "eXtensible Exchange Template (XET) - KSeF, XML, EDI Integration for Odoo 18",
+    "name": "eXtensible Exchange Template (XET) - KSeF, XML, EDI Integration for Odoo 19",
     "summary": "Mapowanie pól modeli Odoo do struktury XML na podstawie XSD + eksport z walidacją.",
     "version": "19.1.6",
     "license": "AGPL-3",
